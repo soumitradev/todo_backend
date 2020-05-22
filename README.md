512e5970d52034a505b8e1c0829649b5a7de16ae
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-05-22 14:22:07 +0530

    Added Update, Delete functionality, Error Handling, and moved to alphanumeric IDs for notes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# ToDo++ [WIP]
+# ToDo++
 ## What?
-This is the next level of my todo list web app. This time, it has a backend written in Python and powered by Flask. It is a CRUD app that uses a RESTful API. This is the code repository for the backend API. It can Create, Read, Update and Delete notes. This *is* a work in progress though, this is a first for me, and this is kind of a big project for me, so this might take some time.
+This is the next level of my todo list web app. This time, it has a backend written in Python and powered by Flask. It is a CRUD app that uses a RESTful API. This is the code repository for the backend API. It can Create, Read, Update and Delete notes. It can store notes publicly, and you can access these notes from any device anywhere in the world. The frontend for this app is still a work in progress, and this code needs to be pushed to a server for this to work.
+
+I'm thinking about hosting the frontend on GitHub Pages or Netlify, but I'll also have to look into where I can host the backend. I hope I can find a place for that. I might host it on GCP's free tier, but I don't know if that much storage would be enough. I'd love it if I could host it on GitHub though.
 
 ## Installation
 ### Requirements
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import flask
 import json
+import random
+import string
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+InvalidID = {
+  "error": {
+    "status": 400,
+    "message": "Bad Request: invalid id"
+  }
+}
+
+EmptyText = {
+  "error": {
+    "status": 400,
+    "message": "Bad Request: empty request"
+  }
+}
 
 @app.route('/', methods=['GET'])
 def home():
@@ -22,31 +38,88 @@ def todo_id():
         results = []
 
         id = flask.request.args.get('id')
-        if id:
-            return str(list(filter(lambda x: x['id'] == int(id), data)))
+        if id in get_taken_ids():
+            return str(list(filter(lambda x: x['id'] == id, data)))
         else:
-            return "Error: Bad Request"
+            return InvalidID, 400
         
-        return json.dumps(results)
     else:
-        with open("./todo.json", "r") as f:
-            data = json.load(f)
+        if flask.request.form['id']:
+            with open("./todo.json", "r") as f:
+                data = json.load(f)
 
-        id = len(data)
+            id = flask.request.form['id']
+            if id in get_taken_ids():
+                title = flask.request.form['title']
+                text = flask.request.form['text']
 
-        print(data)
+                for i in data:
+                    if i['id'] == id:
+                        i['title'] = title
+                        i['text'] = text
+                        res = i
+                        break
 
-        title = flask.request.form['title']
-        text = flask.request.form['text']
+                with open("./todo.json", "w") as f:
+                    json.dump(data, f)
 
-        res = {"id" : id, "title" : title, "text" : text}
-        data.append(res)
+                return json.dumps(res)
+            else:
+                return InvalidID, 400
+        else:
+            with open("./todo.json", "r") as f:
+                data = json.load(f)
 
-        with open("./todo.json", "w") as f:
-            json.dump(data, f)
+            id = get_random_alphaNumeric_string(11)
+            while id in get_taken_ids():
+                id = get_random_alphaNumeric_string(11)
 
-        return json.dumps(res)
+            title = flask.request.form['title']
+            text = flask.request.form['text']
 
+            if title and text:
+                res = {"id" : id, "title" : title, "text" : text}
+                data.append(res)
+
+                with open("./todo.json", "w") as f:
+                    json.dump(data, f)
+
+                return json.dumps(res)
+            else:
+                return EmptyText, 400
+
+@app.route('/api/v1/delete', methods=['POST'])
+def delete_id():
+    with open("./todo.json", "r") as f:
+        data = json.load(f)
+
+    id = flask.request.form['id']
+
+    removed_note = ""
+    for i in data:
+        if i['id'] == id:
+            removed_note = i
+            break
+    
+    if removed_note:
+        data.remove(removed_note)
+    else:
+        return InvalidID, 400
+
+    with open("./todo.json", "w") as f:
+        json.dump(data, f)
+
+    return json.dumps(removed_note)
+
+def get_taken_ids():
+    with open("./todo.json", "r") as f:
+        data = json.load(f)
+
+    return map(lambda x: x['id'], data)
+
+def get_random_alphaNumeric_string(stringLength=8):
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
 
 @app.errorhandler(404)
 def page_not_found(e):
